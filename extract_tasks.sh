ff54c363047e20bfd00be9e8f86983bc7187772a
Laurensdewachter <93316351+Laurensdewachter@users.noreply.github.com>
2021-11-05 16:30:42 +0100

    update script

--- extract_tasks.sh
+++ extract_tasks.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 ARCHIVE=$1
-tar -xvzf $ARCHIVE
-
+tar -xzf $ARCHIVE
+ARCHIVE=$(sed 's/\([^_]*_[^_]*_[^_]*_[^_]*_[^_]*\).tgz/\1/g' <<< $ARCHIVE)
 VAK=$(sed 's/[^_]*_[^_]*_\([^_]*\)_[^_]*_[^_]*/\1/g' <<< $ARCHIVE)
 TAAK=$(sed 's/[^_]*_[^_]*_[^_]*_\([^_]*\)_[^_]*/\1/g' <<< $ARCHIVE)
-mkdir $VAK
+mv $ARCHIVE $VAK
 cd $VAK
 mkdir $TAAK
-cd ..
-cd ..
-ARCHIVE=$(sed 's/\([^_]*_[^_]*_[^_]*_[^_]*_[^_]*\).tgz/\1/g' <<< $ARCHIVE)
-mv /$ARCHIVE/* /$VAK/$TAAK/
+mv *.tgz $TAAK
+cd $TAAK
+AMOUNT_OF_FILES= ls | wc -l
+
