df132877b418a07607e72247b2b1d8d3c1510cb5
laurensdewachter <laurens.dewachter@hotmail.com>
2021-12-09 17:49:47 +0100

    sync

--- Project MIPS/Maze/Read&build.asm
+++ Project MIPS/Maze/Read&build.asm
@@ -1,5 +1,6 @@
 .data
 fin:	.asciiz "input.txt"
+	.align 2
 buffer:	.space 2048
 
 wall:	.asciiz	"w"
@@ -21,10 +22,13 @@ main:
 	
 	subu	$sp, $sp, 12	# adjust stack for 3 items
 	sw	$s2, 12($sp)	# push buffer size to stack
-	sw	$s1, 8($sp)	# push buffer adress
-	sw	$s0, 4($sp)	# push file name
+	sw	$s1, 8($sp)	# push buffer adress to stack
+	sw	$s0, 4($sp)	# push file name adress to stack
 	
 	jal	create
+	
+	li	$v0, 10		# system call for returning to os
+	syscall
 	
 # Procedure to create a bitmap maze based on a .txt file
 create:
@@ -39,13 +43,13 @@ create:
 	sw	$s3, -24($fp)
 	
 	# Load arguments to registers
-	lw	$s0, 4($fp)	# load file name to $s0
+	lw	$s0, 4($fp)	# load file name adress to $s0
 	lw	$s1, 8($fp)	# load buffer adress to $s1
 	lw	$s2, 12($fp)	# load buffer size to $s2
 	
 	# Open file
-	li 	$v0, 13		# System call for opening files
-	move	$a0, $s0	# load file name in $a0
+	li 	$v0, 13		# system call for opening files
+	move	$a0, $s0	# load file name adress in $a0
 	li 	$a1, 0 		# Open for writing
 	li	$a2, 0 		# mode is ignored
 	syscall 		# open a file (file descriptor returned in $v0)
@@ -64,20 +68,30 @@ create:
 	syscall			# close file
 	
 	# Determine width
-	li	$s0, 0		# set $s0 as counter
-	lw	$t1, 0($s1)	# set $t1 to the first character
+	li	$t0, 0		# set $t0 as counter
+	move 	$t1, $s1	# copy buffer location
 width:
-	beq	$s1, 10, width_end	# check if the character is a newline
-	addi	$s0, $s0, 1	# add 1 to counter
-	addi	$s1, $s1, 4	# move to next character
-	j	width
+	lw	$t2, $t1	# load character
+	beq	$t2, 10, width_end	# check if the character is a newline
+	addi	$t0, $t0, 1	# add 1 to counter
+	addi	$t1, $t1, 1	# move to next character
+	j	width		# loop
+	
 width_end:
-	li 	$v0, 1
-	move 	$a0, $t1
-	syscall
+	move	$s2, $t0	# save width to $s2
+	li	$t0, 0		# set $t0 as counter
+	move 	$t1, $s1	# copy buffer location
+height:
+	lw	$t2, ($t1)	# load next character
+	beq	$t2, 0, height_end	# check if the character is not zero
+	addi	$t1, $t1, 1	# move to next character
+	beq	$t2, 10, counter+1	# jump to function that adds 1 to counter
+	j	heigth		# loop
+counter+1:
+	addi	$t0, $t0, 1	# add 1 to counter
+	j	heigth		#loop
+	
+height_end:
 	
 	lw	$ra, -8($fp)
 	jr	$ra
-	
-
-	
