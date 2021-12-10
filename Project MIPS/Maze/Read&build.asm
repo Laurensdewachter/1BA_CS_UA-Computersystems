caf5ea457739eaef4adced2792ec5c230965a133
laurensdewachter <laurens.dewachter@hotmail.com>
2021-12-10 16:56:05 +0100

    update

--- Project MIPS/Maze/Read&build.asm
+++ Project MIPS/Maze/Read&build.asm
@@ -1,15 +1,7 @@
 .data
-fin:	.asciiz "input.txt"
+fin:	.asciiz "/home/laurens/Desktop/Systemen/Project MIPS/Maze/input.txt"
 	.align 2
 buffer:	.space 2048
-
-wall:	.asciiz	"w"
-passage:.asciiz "p"
-player:	.asciiz "s"
-exit:	.asciiz "u"
-newline:.asciiz "\n"
-enemy:	.asciiz "e"
-candy:	.asciiz "c"
 
 .globl main
 
@@ -41,15 +33,17 @@ create:
 	sw	$s1, -16($fp)
 	sw	$s2, -20($fp)
 	sw	$s3, -24($fp)
-	
+
+# Procedure to read a file
+read_file:
 	# Load arguments to registers
-	lw	$s0, 4($fp)	# load file name adress to $s0
-	lw	$s1, 8($fp)	# load buffer adress to $s1
+	lw	$s1, 4($fp)	# load file name adress to $s0
+	lw	$s0, 8($fp)	# load buffer adress to $s1
 	lw	$s2, 12($fp)	# load buffer size to $s2
 	
 	# Open file
 	li 	$v0, 13		# system call for opening files
-	move	$a0, $s0	# load file name adress in $a0
+	move	$a0, $s1	# load file name adress in $a0
 	li 	$a1, 0 		# Open for writing
 	li	$a2, 0 		# mode is ignored
 	syscall 		# open a file (file descriptor returned in $v0)
@@ -58,7 +52,7 @@ create:
 	# Read from file to buffer
 	li 	$v0, 14 	# system call for read from file
 	move 	$a0, $s3 	# file descriptor
-	move 	$a1, $s1	# address of buffer to which to load the contents
+	move 	$a1, $s0	# address of buffer to which to load the contents
 	move 	$a2, $s2 	# hardcoded max number of characters
 	syscall 		# read file
 	
@@ -67,31 +61,67 @@ create:
 	move	$a0, $s3	# file descriptor to close
 	syscall			# close file
 	
+# Procedure 
+determine_width_height:
 	# Determine width
 	li	$t0, 0		# set $t0 as counter
-	move 	$t1, $s1	# copy buffer location
+	move 	$t1, $s0	# copy buffer location
 width:
-	lw	$t2, $t1	# load character
+	lb	$t2, 0($t1)	# load character
 	beq	$t2, 10, width_end	# check if the character is a newline
 	addi	$t0, $t0, 1	# add 1 to counter
 	addi	$t1, $t1, 1	# move to next character
 	j	width		# loop
 	
 width_end:
-	move	$s2, $t0	# save width to $s2
+	move	$s1, $t0	# save width to $s1
 	li	$t0, 0		# set $t0 as counter
-	move 	$t1, $s1	# copy buffer location
+	move 	$t1, $s0	# copy buffer location
 height:
-	lw	$t2, ($t1)	# load next character
+	lb	$t2, ($t1)	# load next character
 	beq	$t2, 0, height_end	# check if the character is not zero
 	addi	$t1, $t1, 1	# move to next character
-	beq	$t2, 10, counter+1	# jump to function that adds 1 to counter
-	j	heigth		# loop
-counter+1:
+	beq	$t2, 10, counter	# jump to function that adds 1 to counter
+	j	height		# loop
+counter:
 	addi	$t0, $t0, 1	# add 1 to counter
-	j	heigth		#loop
+	j	height		#loop
 	
 height_end:
+	addi	$t0, $t0, 1	# count the final row
+	move	$s2, $t0	# store height to $s2
+
+	move	$t0, $s0	# load buffer adress to $t0
+	move	$s3, $gp	# load the bitmap pointer to "s3"
+	li	$t2, 0x0000FF	# load blue to $t2
+	li	$t3, 0x000000	# load black to $t3
+	li	$t4, 0xFFFF00	# load yellow to $t4
+	li	$t5, 0x00FF00	# load green to $t5
+	li	$t6, 0xFF0000	# load red to $t6
+	li	$t7, 0xFFFFFF	# load white to $t7
 	
-	lw	$ra, -8($fp)
+color:
+	lb	$t1, $t0	# load byte to $t1
+	addi	$t0, $t0, 1	# add 1 to "buffer pointer"
+	
+	beq	$t1, 119, wall	# check if the character is a "w"
+	beq	$t1, 112, passage	# check if the character is a "p"
+	beq	$t1, 115, player	# check if the character is a "s"
+	beq	$t1, 117, exit	# check if the character is a "u"
+	beq	$t1, 101, enemy	# check if the character is a "e"
+	beq	$t1, 99, cany	# check if the character is a "c"
+	j	color_end
+	
+wall:
+	sw	$t2, ($s3)	# place blue value at the bitmap "pointer"
+	addi	$s3, $s3, 1	# move bitmap "pointer"
+	j	color
+passage:
+	sw	$t2, ($s3)	# place black value at the bitmap "pointer"
+	addi	$s3, $s3, 1	# move bitmap "pointer"
+	j	color
+player:
+	
+color_end:
+	lw	$ra, -4($fp)
 	jr	$ra
